253c537b2a7a367f8faabe8bb727ae7f23b53411
unknown <sebastian.munoz092@gmail.com>
2024-06-14 11:38:21 -0300

    Subo material ordenamiento burbuja

---
+++ ordenamiento_burbuja.java
@@ -0,0 +1,61 @@
+package ordenamiento_burbuja;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class ordenamiento_burbuja {
+	public static void main(String[] args) {
+		int[] arreglo = { 5, 2, 8, 3, 1 };
+
+		// Mostrar el arreglo desordenado
+		System.out.println("Arreglo desordenado:");
+		for (int num : arreglo) {
+			System.out.print(num + " ");
+		}
+
+		
+		// Aplicar el algoritmo de ordenamiento burbuja
+		
+		
+		for (int i = 0; i < (arreglo.length - 1) /* i < 4 (5-1) */; i++) {
+			 	// 
+			
+			
+			for (int j = 0; j < (arreglo.length - i - 1) ; j++) {
+									
+			/* arreglo.length - i - 1:
+			  	elimina pasadas innecesarias ya que el valor mas alto 
+			  	siempre quedara en la ultima posicion desde el 
+			  	inicio del ordenamiento. 
+			*/
+				// arreglo = { 2, 5, 8, 3, 1 };	
+				
+						
+				if (arreglo[j] > arreglo[j + 1]) {
+					
+					// Intercambiar los elementos
+					int temporal = arreglo[j];
+					// SI TENGO MAS DE UN ARRAY: String temporalNombre = nombre[j];
+					
+					/* temporal sirve para guardar por un momento
+						al valor mas alto. 
+					*/
+					arreglo[j] = arreglo[j + 1];
+					// SI TENGO MAS DE UN ARRAY: nombre[j] =  nombre[j+1]
+					
+					// La primera posicion de las dos, toma el valor de la segunda (que es mas chico).
+					arreglo[j + 1] = temporal;
+					// SI TENGO MAS DE UN ARRAY: nombre[j+1] = temporalNombre ;
+					
+					// La segunda posicion, toma el valor mas alto.
+				}
+			}
+		}
+
+		// Mostrar el arreglo ordenado
+		System.out.println("\nArreglo ordenado:");
+		for (int num : arreglo) {
+			System.out.print(num + " ");
+		}
+	}
+}
